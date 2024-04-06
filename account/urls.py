da9d99f445267dfaa016d4941eaea74db402a211
mohadeseh shoja <mohadeseh.shoja2020@gmail.com>
2024-04-07 00:39:29 +0430

    today work...

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,8 @@
-from django.urls import path
-urlpatterns=[
 
+from django.urls import path
+
+from account import views
+from account.views import ProfileList
+urlpatterns=[
+    path('profileposts/',views.ProfileList.as_view(),name='profile_list')
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from account.models import Profile
+from account.serializers import ProfileSerializer
 # Create your views here.
+class ProfileList(generics.ListCreateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
